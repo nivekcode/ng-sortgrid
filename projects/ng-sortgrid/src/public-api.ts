84ccf38394864e2d28f62a539d150f6241a0dc3e
kreuzerk <kevin.kreuzer90@icloud.com>
2022-03-03 09:21:27 +0100

    fix: 🐛 (draghandle) export drag handle directive

--- projects/ng-sortgrid/src/public-api.ts
+++ projects/ng-sortgrid/src/public-api.ts
@@ -1,6 +1,7 @@
 /*
  * Public API Surface of ng-sortgrid
  */
-export * from './lib/ngsg-item.directive';
 export * from './lib/ngsg.module';
+export * from './lib/ngsg-item.directive';
+export * from './lib/ngsg-drag-handle.directive';
 export * from './lib/shared/ngsg-order-change.model';
